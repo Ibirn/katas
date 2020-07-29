4b97c307165c8cf00c5f4c0dcc37d06c5511726f
Ibirn <kmcricciuti@gmail.com>
2020-07-29 16:12:39 +0000

    FINALLY CRACKED IT. Don't forget basics, like just setting things to equal something, rather than cycling and appending.

---
+++ kata15.2.js
@@ -0,0 +1,29 @@
+const stringSplit = (input, cut) => {
+  let output = [];
+  for (let i = 0; i < input.length; i += cut) {
+    output.push(input.substr(i, cut));
+  }
+  return output;
+}
+
+const squareCode = (message) => {
+  message = message.split(' ').join('')
+  let longth = Math.ceil(Math.sqrt(message.length))
+  let codeSeg = stringSplit(message, longth);
+  let output = [];
+  for (let k = 0; k <= codeSeg.length; k++){
+    output[k] = '';
+  }
+  for (let i = 0; i < codeSeg.length; i++) {
+    for (let j = 0; j < codeSeg[i].length; j++) {
+      output[j] += codeSeg[i][j];
+    }
+  }
+  output = output.join(' ');
+  return output;
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
